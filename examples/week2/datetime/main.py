07e3a44a787074e7092e78235332feab89679cf6
Professor Laney <denalaneywa@gmail.com>
2026-09-20 10:00:47 -0700

    Delete examples/week2/datetime directory

--- examples/week2/datetime/main.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-current_datetime = datetime.now()
-
-print("Current date and time:", current_datetime)
-
-print("Current date:", current_datetime.strftime("%m/%d/%Y"))
-print("Current time:", current_datetime.strftime("%I:%M %p"))
